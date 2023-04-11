b6d736d7807c628c515c14f801c1836a0c4b7af1
Muhamet <muhametsadiku655@gmail.com>
2023-04-11 10:54:22 +0200

    getStaticProps

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "@/components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg"
+      title="A first meetup"
+      address="Some Street 5, some city"
+      description="this is a first meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
